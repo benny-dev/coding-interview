377151b14a1d2ca21100d35c678b9d8550aa6a9d
Benjamin Asante <benjaminasante191@gmail.com>
2022-03-04 18:50:31 +0000

    added reverse string with decremental for loop

--- string-reversal.js
+++ string-reversal.js
@@ -34,3 +34,31 @@ this is called method chaining
 
 //test case with goodbye. expected resutl is eybdoog
 console.log(reverStringWithBuiltInMethods("goodbye"));
+
+/* 
+  The second way which is a decrementing for loop is going to take the last character of the string
+  and store it in a new variable and add on to it the subsequent characters till the word is exhausted.
+  this can be achieved by the basic for loop conditional
+*/
+
+function reverseStringWithDecrementalForLoop(str) {
+  //we have an empty string variable which we would push the reverse characters in.
+  let reversedString = "";
+
+  /* 
+    here, we are setting the loop counter to the last character of the string ie let i = str.length - 1; 
+    -1 because strings are based based index.
+    then we start our loop from the lenght of the string down to zero and decrease the value of the counter after each iteration.
+  */
+  for (let i = str.length - 1; i >= 0; i--) {
+    //strings behave like arrays, but they are not arrays. so which such behave we can get the value of a particular index.
+    // we are saving that value in the empty declared string reverseString += str[i] is the same as reverseString = reversedString + str[i] (string concatenation)
+    //console.log(str[i]) //you can uncomment this to see the value of the char each the loop runs
+    reversedString += str[i];
+  }
+
+  //this is going to run after the loop condition is over and return the reversed string
+  return reversedString;
+}
+
+console.log(reverseStringWithDecrementalForLoop("goodbye"));
